4bb13f9992edf5a4054a0dafe59d292a33ab5056
gyuoo <yeomky98@naver.com>
2024-11-11 01:38:58 +0900

    feat(exception): add error messages

--- src/main/java/store/exception/enums/ErrorMessage.java
+++ src/main/java/store/exception/enums/ErrorMessage.java
@@ -1,7 +1,13 @@
 package store.exception.enums;
 
 public enum ErrorMessage {
-    ;
+    INVALID_FORMAT("올바르지 않은 형식으로 입력했습니다."),
+    PRODUCT_NOT_FOUND("존재하지 않는 상품입니다."),
+    EMPTY_PRODUCT_NAME("상품명은 비어 있을 수 없습니다."),
+    INVALID_QUANTITY_FORMAT("수량은 숫자 형식이어야 합니다."),
+    INVALID_QUANTITY_AMOUNT("수량은 1 이상이어야 합니다."),
+    EXCEED_STOCK_QUANTITY("재고 수량을 초과하여 구매할 수 없습니다."),
+    INVALID_COMMON_INPUT("잘못된 입력입니다.");
 
     private final String message;
 
