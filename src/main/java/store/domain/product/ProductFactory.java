c8f250973282060003cb1b03ee1491bd42ac35f2
gyuoo <yeomky98@naver.com>
2024-11-10 23:28:05 +0900

    refactor(product): simplify product creation logic in ProductFactory

--- src/main/java/store/domain/product/ProductFactory.java
+++ src/main/java/store/domain/product/ProductFactory.java
@@ -5,9 +5,9 @@ import store.domain.promotion.Promotion;
 public class ProductFactory {
 
     public static Product createProduct(String name, int price, int quantity, Promotion promotion) {
-        if (promotion != null) {
-            return new PromotionProduct(name, price, quantity, promotion);
+        if (promotion == null) {
+            return new Product(name, price, quantity);
         }
-        return new Product(name, price, quantity);
+        return new PromotionProduct(name, price, quantity, promotion);
     }
 }
