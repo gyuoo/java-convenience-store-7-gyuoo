dd4781a93686d116114bac641261b0d3832ccefe
gyuoo <yeomky98@naver.com>
2024-11-11 02:32:11 +0900

    feat(view): add re-prompt on validation failure

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,15 +1,21 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import store.domain.product.ProductInformation;
 import store.util.ProductInputValidator;
+import store.util.StockInputValidator;
 
 public class InputView {
 
     private final OutputView outputView;
+    private final StockInputValidator stockInputValidator;
     private final ProductInputValidator productInputValidator;
 
-    public InputView(OutputView outputView, ProductInputValidator productInputValidator) {
+    public InputView(OutputView outputView, StockInputValidator stockInputValidator,
+                     ProductInputValidator productInputValidator) {
         this.outputView = outputView;
+        this.stockInputValidator = stockInputValidator;
         this.productInputValidator = productInputValidator;
     }
 
@@ -17,12 +23,14 @@ public class InputView {
         return Console.readLine();
     }
 
-    public String askForProductsToPurchase() {
+    public List<ProductInformation> askForProductsToPurchase() {
         while (true) {
             outputView.printEnterProductsToPurchase();
             String input = readLine();
             try {
-                return productInputValidator.validateProductInput(input);
+                List<ProductInformation> products = productInputValidator.validateProductInput(input);
+                stockInputValidator.validateProductDetails(products);
+                return products;
             } catch (IllegalArgumentException e) {
                 outputView.printMessage(e.getMessage());
             }
