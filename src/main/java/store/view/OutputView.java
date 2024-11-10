fd477edbab7996b6a3a8fd6649f87867ea423fc2
gyuoo <yeomky98@naver.com>
2024-11-11 01:22:18 +0900

    feat(view): add printing method for welcome

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,9 +1,14 @@
 package store.view;
 
+import static store.view.ConsoleMessage.WELCOME_MESSAGE;
+
 public class OutputView {
 
     public void printMessage(String message) {
         System.out.println(message);
     }
-    
+
+    public void printWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+    }
 }
