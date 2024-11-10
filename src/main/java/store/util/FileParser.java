8e00030fc09aba48f38998f4bcea30f89705ed77
gyuoo <yeomky98@naver.com>
2024-11-10 23:57:29 +0900

    feat(util): add parser for parsing promotions

--- src/main/java/store/util/FileParser.java
+++ src/main/java/store/util/FileParser.java
@@ -1,9 +1,12 @@
 package store.util;
 
 import static store.domain.product.ProductFactory.createProduct;
+import static store.domain.promotion.PromotionFactory.createPromotion;
 
+import java.time.LocalDate;
 import java.util.List;
 import store.domain.product.Product;
+import store.domain.promotion.Promotion;
 import store.domain.promotion.PromotionRepository;
 
 public class FileParser {
@@ -21,5 +24,15 @@ public class FileParser {
         String promotion = values.get(3);
 
         return createProduct(name, price, quantity, promotionRepository.getPromotionByName(promotion));
+    }
+
+    public Promotion parsePromotion(List<String> values) {
+        String name = values.get(0);
+        int buy = Integer.parseInt(values.get(1));
+        int get = Integer.parseInt(values.get(2));
+        LocalDate startDate = LocalDate.parse(values.get(3));
+        LocalDate endDate = LocalDate.parse(values.get(4));
+
+        return createPromotion(name, buy, get, startDate, endDate);
     }
 }
