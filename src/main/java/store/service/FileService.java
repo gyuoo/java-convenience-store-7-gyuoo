0c44a3af9a9d8362953b909748a5c047d427d7ad
gyuoo <yeomky98@naver.com>
2024-11-10 23:52:00 +0900

    refactor(service): streamline file loading and parsing in FileService

--- src/main/java/store/service/FileService.java
+++ src/main/java/store/service/FileService.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import store.domain.product.Product;
 import store.domain.product.ProductRepository;
 import store.domain.promotion.Promotion;
@@ -26,41 +27,31 @@ public class FileService {
     }
 
     public void savePromotions(List<Promotion> promotions) {
-        for (Promotion promotion : promotions) {
-            promotionRepository.save(promotion);
-        }
-    }
-
-    private List<Promotion> readPromotions(String filePath) throws IOException {
-        List<Promotion> promotions = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            skipHeader(br);
-            String line;
-            while ((line = br.readLine()) != null) {
-                Promotion promotion = fileParser.parsePromotion(parseLine(line));
-                promotions.add(promotion);
-            }
-        }
-        return promotions;
+        promotions.forEach(promotionRepository::save);
     }
 
     public void saveProducts(List<Product> products) {
-        for (Product product : products) {
-            productRepository.save(product);
-        }
+        products.forEach(productRepository::save);
     }
 
-    private List<Product> readProducts(String filePath) throws IOException {
-        List<Product> products = new ArrayList<>();
+    public List<Promotion> loadPromotionsFromFile(String filePath) throws IOException {
+        return loadEntitiesFromFile(filePath, line -> fileParser.parsePromotion(parseLine(line)));
+    }
+
+    public List<Product> loadProductsFromFile(String filePath) throws IOException {
+        return loadEntitiesFromFile(filePath, line -> fileParser.parseProduct(parseLine(line)));
+    }
+
+    private <T> List<T> loadEntitiesFromFile(String filePath, Function<String, T> parseFunction) throws IOException {
+        List<T> entities = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             skipHeader(br);
             String line;
             while ((line = br.readLine()) != null) {
-                Product product = fileParser.parseProduct(parseLine(line));
-                products.add(product);
+                entities.add(parseFunction.apply(line));
             }
         }
-        return products;
+        return entities;
     }
 
     private void skipHeader(BufferedReader br) throws IOException {
