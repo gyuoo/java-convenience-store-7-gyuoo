87d3e61acf0cb371626ecc6a812edb2b9cc7bf51
gyuoo <yeomky98@naver.com>
2024-11-11 02:27:10 +0900

    refactor(util): remove unnecessary codes

--- src/main/java/store/util/ProductInputValidator.java
+++ src/main/java/store/util/ProductInputValidator.java
@@ -5,6 +5,8 @@ import static store.exception.enums.ErrorMessage.INVALID_FORMAT;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import store.domain.product.ProductInformation;
 
 public class ProductInputValidator {
 
@@ -14,33 +16,33 @@ public class ProductInputValidator {
     private static final String CLOSE_BRACKET = "]";
     private static final String SPACE = " ";
 
-    public String validateProductInput(String input) {
+    public List<ProductInformation> validateProductInput(String input) {
         if (isNullOrEmpty(input)) {
             throw new IllegalArgumentException(EMPTY_INPUT.getMessage());
         }
         List<String> products = Arrays.stream(input.split(PRODUCT_SEPARATOR)).toList();
-        for (String product : products) {
-            if (!isValidProductFormat(product.trim())) {
-                throw new IllegalArgumentException(INVALID_FORMAT.getMessage());
-            }
-        }
-        return input;
+        return products.stream()
+                .map(this::parseProductInformation)
+                .collect(Collectors.toList());
     }
 
-    private boolean isNullOrEmpty(String input) {
-        return input == null || input.isEmpty();
-    }
-
-    private boolean isValidProductFormat(String product) {
-        if (!isWrappedWithBrackets(product)) {
+    private ProductInformation parseProductInformation(String product) {
+        if (!isWrappedWithBrackets(product.trim())) {
             throw new IllegalArgumentException(INVALID_FORMAT.getMessage());
         }
         String content = removeBrackets(product);
         List<String> parts = splitNameAndQuantity(content);
 
-        validateProductName(parts.get(0));
-        validateProductQuantity(parts.get(1));
-        return true;
+        String name = removeSpace(parts.get(0));
+        validateProductName(name);
+        String quantity = removeSpace(parts.get(1));
+        validateProductQuantity(quantity);
+
+        return new ProductInformation(name, Integer.parseInt(quantity));
+    }
+
+    private boolean isNullOrEmpty(String input) {
+        return input == null || input.isEmpty();
     }
 
     private String removeBrackets(String input) {
