427ed02dd4ed18172e61d020b97ab4a69de87dfe
염규영 <yeomky98@naver.com>
2024-11-11 20:29:11 +0900

    feat(view): add user confirmation method with input validation

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,6 +3,7 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import store.domain.product.ProductInformation;
+import store.global.exception.enums.ErrorMessage;
 import store.global.util.ProductInputValidator;
 import store.global.util.StockInputValidator;
 
@@ -38,4 +39,17 @@ public class InputView {
         }
     }
 
+    public boolean userConfirmed(String message) {
+        while (true) {
+            System.out.println(message);
+            String response = readLine();
+            if (response.equals("Y")) {
+                return true;
+            }
+            if (response.equals("N")) {
+                return false;
+            }
+            System.out.println(ErrorMessage.INVALID_COMMON_INPUT.getMessage());
+        }
+    }
 }
