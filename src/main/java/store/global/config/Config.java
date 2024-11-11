d773551cda747d24a28db5ee0d5478046f1b82dd
gyuoo <yeomky98@naver.com>
2024-11-11 23:54:33 +0900

    feat(config): update Config

--- src/main/java/store/global/config/Config.java
+++ src/main/java/store/global/config/Config.java
@@ -2,8 +2,13 @@ package store.global.config;
 
 import store.controller.MainController;
 import store.controller.OrderController;
+import store.controller.PaymentController;
+import store.domain.discount.DiscountCalculator;
+import store.domain.discount.MembershipDiscount;
+import store.domain.discount.PromotionDiscount;
 import store.domain.order.OrderManager;
 import store.domain.product.ProductRepository;
+import store.domain.promotion.Promotion;
 import store.domain.promotion.PromotionRepository;
 import store.global.util.FileParser;
 import store.global.util.ProductInputValidator;
@@ -19,21 +24,22 @@ public class Config {
     private final PromotionRepository promotionRepository = new PromotionRepository();
     private final ProductRepository productRepository = new ProductRepository();
     private final FileParser fileParser = new FileParser(promotionRepository);
-    private final FileService fileService = new FileService(fileParser, productRepository,
-        promotionRepository);
+    private final FileService fileService = new FileService(fileParser, productRepository, promotionRepository);
     private final ProductInputValidator productInputValidator = new ProductInputValidator();
-    private final StockInputValidator stockInputValidator = new StockInputValidator(
-        productRepository);
+    private final StockInputValidator stockInputValidator = new StockInputValidator(productRepository);
     private final StockMessageChecker stockMessageChecker = new StockMessageChecker();
     private final OutputFormatter outputFormatter = new OutputFormatter();
     private final OutputView outputView = new OutputView(outputFormatter, productRepository);
-    private final InputView inputView = new InputView(outputView, stockInputValidator,
-        productInputValidator);
-    private final OrderManager orderManager = new OrderManager(productRepository,
-        outputView, inputView);
+    private final InputView inputView = new InputView(outputView, stockInputValidator, productInputValidator);
+    private final MembershipDiscount membershipDiscount = new MembershipDiscount();
+    private final PromotionDiscount promotionDiscount = new PromotionDiscount(new Promotion());
+    private final DiscountCalculator discountCalculator = new DiscountCalculator(new Promotion());
+    private final OrderManager orderManager = new OrderManager(productRepository, outputView, inputView,
+            discountCalculator);
+    private final PaymentController paymentController = new PaymentController(inputView, outputView, productRepository);
     private final OrderController orderController = new OrderController(orderManager);
-    private final MainController controller = new MainController(fileService, inputView, outputView,
-        orderController);
+    private final MainController controller = new MainController(fileService, inputView, outputView, orderManager,
+            orderController, paymentController);
 
     public MainController mainController() {
         return controller;
