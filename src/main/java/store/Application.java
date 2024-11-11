05a1ddd654b03b56475611d5e391aebb78192231
염규영 <yeomky98@naver.com>
2024-11-11 20:00:19 +0900

    feat(application): update Application

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,14 @@
 package store;
 
+import java.io.IOException;
+import store.controller.MainController;
+import store.global.config.Config;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    public static void main(String[] args) throws IOException {
+        Config config = new Config();
+        MainController controller = config.mainController();
+        controller.run();
     }
 }
