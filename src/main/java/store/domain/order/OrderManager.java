5afde8a856c5f0a57c1b5dae76404a6a6b54e0cb
gyuoo <yeomky98@naver.com>
2024-11-11 23:53:29 +0900

    feat(order): add method to get OrderItem list

--- src/main/java/store/domain/order/OrderManager.java
+++ src/main/java/store/domain/order/OrderManager.java
@@ -1,6 +1,8 @@
 package store.domain.order;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import store.domain.discount.DiscountCalculator;
 import store.domain.product.ProductInformation;
 import store.domain.product.ProductRepository;
 import store.view.InputView;
@@ -11,19 +13,29 @@ public class OrderManager {
     private final ProductRepository productRepository;
     private final OutputView outputView;
     private final InputView inputView;
+    private final DiscountCalculator discountCalculator;
 
     public OrderManager(ProductRepository productRepository, OutputView outputView,
-        InputView inputView) {
+                        InputView inputView, DiscountCalculator discountCalculator) {
         this.productRepository = productRepository;
         this.outputView = outputView;
         this.inputView = inputView;
+        this.discountCalculator = discountCalculator;
     }
+
+    public List<OrderItem> createOrderItems(List<ProductInformation> userProducts) {
+        return userProducts.stream()
+                .map(info -> new OrderItem(productRepository.findProduct(info.getName()),
+                        info.getQuantity()))
+                .collect(Collectors.toList());
+    }
+
 
     public Order createOrder(List<ProductInformation> userProducts) {
         List<OrderItem> items = userProducts.stream()
-            .map(info -> new OrderItem(productRepository.findProduct(info.getName()),
-                info.getQuantity()))
-            .toList();
-        return new Order(items, productRepository, outputView, inputView);
+                .map(info -> new OrderItem(productRepository.findProduct(info.getName()),
+                        info.getQuantity()))
+                .toList();
+        return new Order(items, productRepository, outputView, inputView, discountCalculator);
     }
 }
