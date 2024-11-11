d31685d451ce1e9b543218c94c4e46ea027d7f94
염규영 <yeomky98@naver.com>
2024-11-11 19:59:56 +0900

    feat(config): create class for centralized dependency management

---
+++ src/main/java/store/global/config/Config.java
@@ -0,0 +1,36 @@
+package store.global.config;
+
+import store.controller.MainController;
+import store.domain.product.ProductRepository;
+import store.domain.promotion.PromotionRepository;
+import store.global.util.FileParser;
+import store.global.util.ProductInputValidator;
+import store.global.util.StockInputValidator;
+import store.service.FileService;
+import store.view.InputView;
+import store.view.OutputFormatter;
+import store.view.OutputView;
+import store.view.StockMessageChecker;
+
+public class Config {
+
+    private final PromotionRepository promotionRepository = new PromotionRepository();
+    private final ProductRepository productRepository = new ProductRepository();
+    private final FileParser fileParser = new FileParser(promotionRepository);
+    private final FileService fileService = new FileService(fileParser, productRepository,
+        promotionRepository);
+    private final ProductInputValidator productInputValidator = new ProductInputValidator();
+    private final StockInputValidator stockInputValidator = new StockInputValidator(
+        productRepository);
+    private final StockMessageChecker stockMessageChecker = new StockMessageChecker();
+    private final OutputFormatter outputFormatter = new OutputFormatter();
+    private final OutputView outputView = new OutputView(outputFormatter, productRepository);
+    private final InputView inputView = new InputView(outputView, stockInputValidator,
+        productInputValidator);
+    private final MainController controller = new MainController(fileService, inputView,
+        outputView);
+
+    public MainController mainController() {
+        return controller;
+    }
+}
