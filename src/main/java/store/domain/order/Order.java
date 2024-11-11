6047f39d7217704c193fba70b144e12d43a9c667
gyuoo <yeomky98@naver.com>
2024-11-11 23:53:08 +0900

    feat(order): add methods to get receipt

--- src/main/java/store/domain/order/Order.java
+++ src/main/java/store/domain/order/Order.java
@@ -1,6 +1,7 @@
 package store.domain.order;
 
 import java.util.List;
+import store.domain.discount.DiscountCalculator;
 import store.domain.product.ProductRepository;
 import store.view.ConsoleMessage;
 import store.view.InputView;
@@ -12,13 +13,15 @@ public class Order {
     private final ProductRepository productRepository;
     private final OutputView outputView;
     private final InputView inputView;
+    private final DiscountCalculator discountCalculator;
 
     public Order(List<OrderItem> orderItems, ProductRepository productRepository,
-        OutputView outputView, InputView inputView) {
+                 OutputView outputView, InputView inputView, DiscountCalculator discountCalculator) {
         this.orderItems = orderItems;
         this.productRepository = productRepository;
         this.outputView = outputView;
         this.inputView = inputView;
+        this.discountCalculator = discountCalculator;
     }
 
     public void processItems() {
@@ -40,7 +43,7 @@ public class Order {
     private void processItem(OrderItem item) {
         int promoStock = productRepository.getPromotionStock(item.getProductName());
         if (isPartialPromotionAvailable(item, promoStock) && !handlePartialPromotion(item,
-            promoStock)) {
+                promoStock)) {
             return;
         }
         productRepository.reduceStock(item.getProductName(), item.getQuantity());
@@ -53,10 +56,28 @@ public class Order {
     private boolean handlePartialPromotion(OrderItem item, int promotionStock) {
         int nonPromotionQuantity = item.getQuantity() - promotionStock;
         String message = String.format(
-            ConsoleMessage.PROMOTION_STOCK_INSUFFICIENT.getMessage(),
-            item.getProductName(), nonPromotionQuantity
+                ConsoleMessage.PROMOTION_STOCK_INSUFFICIENT.getMessage(),
+                item.getProductName(), nonPromotionQuantity
         );
-        return inputView.userConfirmed(message);
+        return inputView.askForPurchaseWithoutPromotion();
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+//    public Receipt getReceipt() {
+//        int totalAmount = discountCalculator.calculateTotalPrice();
+//        int promotionDiscountAmount = calculateDiscountedQuantity();
+//        int membershipDiscountAmount = membershipDiscount.applyMembershipDiscount();
+//        return new Receipt(orderItems, promotionDiscountAmount, membershipDiscountAmount, totalAmount);
+//    }
+
+    private int calculateTotalAmount() {
+        return orderItems.stream()
+                .mapToInt(
+                        item -> item.getQuantity() * productRepository.findProduct(item.getProductName())
+                                .getPrice())
+                .sum();
     }
 }
-
